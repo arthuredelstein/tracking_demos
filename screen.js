8b1d63294a3da3ba41fed396a0cfc77702b87558
Arthur Edelstein <arthuredelstein@gmail.com>
2022-05-31 16:22:45 -0700

    screen demo

---
+++ screen.js
@@ -0,0 +1,51 @@
+const apis = [
+    "window.screenX",
+    "window.screenY",
+    "window.outerWidth",
+    "window.outerHeight",
+    "window.outerWidth - window.innerWidth",
+    "window.outerHeight - window.innerHeight",
+    "window.screen.width",
+    "window.screen.height",
+    "window.screen.availWidth",
+    "window.screen.availHeight",
+    "window.screen.availLeft",
+    "window.screen.availHeight",
+    "window.screen.isExtended",
+];
+
+let result = "";
+for (const api of apis) {
+    result += `${api}: ${eval(api)}\n`;
+}
+
+const lessThan = (feature, val) => !matchMedia(`(min-${feature}: ${val}px)`).matches;
+
+const find = (min, max, feature) => {
+    if (min === max) {
+	return min;
+    }
+    const mid = Math.ceil((max + min)/2);
+    console.log(min, mid, max, lessThan(feature, mid));
+    return lessThan(feature, mid) ?
+	find(min, mid - 1, feature) : find(mid, max, feature);
+};
+
+result += `device-width (media query): ${find(0, 10000, "device-width")}\n`;
+result += `device-height (media query): ${find(0, 10000, "device-height")}\n`;
+
+const resultDiv = document.getElementById("result");
+resultDiv.innerText = result;
+
+
+document.addEventListener("mousemove", e => {
+    console.log(e);
+    result += `mouseEvent.screenX: ${e.screenX}\n`;
+    result += `mouseEvent.screenY: ${e.screenY}\n`;
+    result += `mouseEvent.screenX - mouseEvent.clientX - (window.outerWidth - window.innerWidth): ${e.screenX - e.clientX - (outerWidth - innerWidth)}\n`;
+    result += `mouseEvent.screenY - mouseEvent.clientY - (window.outerHeight - window.innerHeight): ${e.screenY - e.clientY - (outerHeight - innerHeight)}\n`;
+    resultDiv.innerText = result;
+}, { once: true} ); 
+
+
+console.log("done");
